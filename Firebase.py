a85bb31abad313e65758e4db9717d2ed941dfba7
Pranesh-12345 <125364788+Pranesh-12345@users.noreply.github.com>
2023-12-26 10:47:23 +0530

    Update Firebase.py

--- Firebase.py
+++ Firebase.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials, db
 import time
 
 # Replace with your Firebase project credentials
-cred = credentials.Certificate("/home/pi/Desktop/git")
+cred = credentials.Certificate("/home/pi/Desktop/git/fir-demo-c7e7a-firebase-adminsdk-ettih-ec76f9b27c.json")
 firebase_admin.initialize_app(cred, {'databaseURL': 'https://fir-demo-c7e7a-default-rtdb.firebaseio.com/'})
 
 # Define the path in the Firebase Realtime Database
@@ -21,23 +21,23 @@ while True:
         # Get data from Firebase
         data0 = db.reference(belt).get()
         data1= db.reference(current).get()
-        data2= db.reference(rpm).get()
-        data3= db.reference(sound).get()
-        data4= db.reference(position).get()
+        data2= db.reference(position).get()
+        data3= db.reference(rpm).get()
+        data4= db.reference(sound).get()
         data5= db.reference(temp).get()
         data6 = db.reference(vis).get()
         data7 = db.reference(volt).get()
         
         if data0 is not None:
             # Process the received data
-            print("Received Data:", data0)
-            print("Received Data:", data1)
-            print("Received Data:", data2)
-            print("Received Data:", data3)
-            print("Received Data:", data4)
-            print("Received Data:", data5)
-            print("Received Data:", data6)
-            print("Received Data:", data7)
+            print("Belt:", data0)
+            print("Current:", data1)
+            print("Position:", data2)
+            print("RPM:", data3)
+            print("Sound Decibel:", data4)
+            print("Temperature:", data5)
+            print("Viscosity:", data6)
+            print("Voltage:", data7)
 
             # Add your logic here to perform actions based on the received data
 
@@ -46,4 +46,4 @@ while True:
     except Exception as e:
         print("Error:", e)
 
-    time.sleep(1)  # Adjust the delay based on your requirements
+    time.sleep(0.5)  # Adjust the delay based on your requirements
